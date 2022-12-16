11f3f8e79126de82d35416c61734195a67ecd066
Emrecangok <115079415+Emrecangok@users.noreply.github.com>
2022-12-16 20:28:16 +0300

    Add files via upload
    Deneme.

---
+++ test_ekleme_cıkarma(Beta)/addq.h
@@ -0,0 +1,120 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define enfazla_uzunluk 1000
+
+char* dosya_adı[100];
+char soru[300];
+char sık[300];
+
+void soru_dosyası_olustur() {
+
+	FILE* dosya;
+	
+	char gecicidosyadadı[]="yemek        ";
+	
+	//*ad = *strcat(ad,".txt");
+	printf("%s",strcat(gecicidosyadadı,".txt"));
+	//dosya = fopen(,"w");
+	
+	
+
+
+
+
+}
+void test_ekle(char tur) {
+
+	
+int soru_sayisi;
+
+system("cls");
+printf("DıKKAT Her sorunun 4 sikki olmak zorundadir\n");
+printf("%c. turdeki testiniz kac sorudan olusacak?\n",tur);
+scanf("%d",&soru_sayisi);
+
+
+printf("Test adini girinizz...\n");
+scanf("%s",&dosya_adı);
+
+
+
+for (char i = 0; i <soru_sayisi ; i++)
+{
+	
+		printf("Lutfen %c . sorunuzu giriniz\n",(i+1));
+		scanf("%s",&soru);
+	//	fprintf(soru_dosyası_olustur(*dosya_adı),"%s\n",soru);
+		soru_dosyası_olustur();
+
+	
+		
+	for (char j = 0; j < 4; j++)
+	{
+		printf("%c. sorunun %c . sıkkini giriniz",(i+1),(j+1));
+		scanf("%s", &sık);
+		//fprintf(soru_dosyası_olustur(*dosya_adı),"%s",sık);
+		
+
+
+	}
+
+}
+
+
+
+
+}
+
+
+void test_turusec() {
+
+	char eklenecek_tur=0;
+	
+	FILE *turler;
+	
+
+	turler = fopen("turler.txt","r");
+
+	char satır[enfazla_uzunluk];
+
+	if (turler == NULL)
+	{
+		perror("Dosya bulunamadi");
+		exit(1);
+	}
+	
+	
+		while (fgets(satır,enfazla_uzunluk,turler))
+		{
+			printf("%s",satır);
+			
+
+		}
+printf("\nYukaridaki turler arasindan hangisine test ekelmek istersiniz(1-2-3...)\n");
+
+scanf("%s",&eklenecek_tur);	
+			
+		
+	
+
+
+	switch (eklenecek_tur)
+	{
+	case '1': {
+		test_ekle(eklenecek_tur);
+		
+	
+	
+	
+	}
+	case '2':
+
+	default:printf("Gecerli bir deger giriniz");
+
+		break;
+	}
+
+
+}
---
+++ test_ekleme_cıkarma(Beta)/main.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"addq.h"
+
+
+#define tane 3
+#define uzunluk 15
+
+void yaz(char* ad)
+{
+	printf("%s",ad);
+
+}
+
+int main() {
+
+
+
+
+#pragma region Dosya acma
+  /*  FILE* dosya;
+	dosya =fopen("C:\\Users\\Emrecan\\Desktop\\yeni.txt","w");*/
+#pragma endregion
+
+#pragma region Veri yazma islemi
+
+	//FILE *dosya0;
+
+	//dosya0 = fopen("C:\\Users\\Emrecan\\Desktop\\yazdirma.txt","w");
+
+	////putc("adsadsad",dosya0);
+	////putc(veri,hangidosya)
+	////putc("\n",dosya0);
+	//fprintf(dosya0,"%s","Emrecan Gok");
+	//fclose(dosya0);
+	////dosyadaki okuma islemini kapatýr
+
+
+
+#pragma endregion
+
+#pragma region okuma
+
+	/*FILE* dosya1;
+	dosya1 = fopen("C:\\Users\\Emrecan\\Desktop\\yazdirma.txt", "r");*/
+
+#pragma endregion
+
+	
+
+	char secilen;
+	printf("test turu seciniz(1,2,3)\n");
+	scanf("%c",&secilen);
+
+	switch (secilen)
+	{
+	case '1': { system("cls"); printf("1. turu sectiniz\n"); break;
+	}
+	case '2': {system("cls"); printf("2. turu sectiniz\n"); break;}
+	case'3': { test_turusec(); break;}
+	
+
+
+
+	default:printf("gecerli bir deger girin");break;
+		
+	}
+
+
+
+
+
+	
+
+
+	return 0;
+
+}
